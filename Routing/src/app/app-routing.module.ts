f0c8aa4481490ae225db4b64d656cefbf69037a2
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2023-05-08 01:21:51 +0530

    .

--- Routing/src/app/app-routing.module.ts
+++ Routing/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BatchesComponent } from './batches/batches.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { InvalidComponent } from './invalid/invalid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'batches',component:BatchesComponent},    //Redering Batches Component
+  {path:'aboutus',component:AboutusComponent},    //Redering AboutUS Component
+  {path:'subjects',component:SubjectsComponent},  //Redering Subjects Component
+  {path:'',component:AboutusComponent},           //This is Default route
+  {path:'**',component:InvalidComponent}          //This is Wild card route this should declared be at the end 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BatchesComponent } from './batches/batches.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { InvalidComponent } from './invalid/invalid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BatchesComponent,
+    SubjectsComponent,
+    AboutusComponent,
+    InvalidComponent
   ],
   imports: [
     BrowserModule,
