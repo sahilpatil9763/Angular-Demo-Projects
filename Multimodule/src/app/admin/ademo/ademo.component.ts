87a29cdf587c754a7e1a91945035d30e8d0fb958
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2023-05-08 18:22:47 +0530

    .

---
+++ Multimodule/src/app/admin/ademo/ademo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ademo',
+  templateUrl: './ademo.component.html',
+  styleUrls: ['./ademo.component.css']
+})
+export class AdemoComponent {
+
+}
---
+++ Multimodule/src/app/student/sdemo/sdemo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sdemo',
+  templateUrl: './sdemo.component.html',
+  styleUrls: ['./sdemo.component.css']
+})
+export class SdemoComponent {
+
+}
